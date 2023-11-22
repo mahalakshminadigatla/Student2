560ffe4a40b71c070fc8f551b99e0d9d60dd47d3
mahalakshminadigatla <mahalakshminadigatla2001@gmail.com>
2023-11-22 18:57:58 +0530

    Commit Message 1

--- Student2/src/test/java/com/example/Student2/Student2ApplicationTests.java
+++ Student2/src/test/java/com/example/Student2/Student2ApplicationTests.java
@@ -11,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -21,12 +25,35 @@ class Student2ApplicationTests {
 		private StudentService studentService;
 
 
-		@Test
-		public void addStudentTest(){
-		Student student = new Student(1,"Maha", 22, 30000);
+	@Test
+	public void addStudentTest() {
+		Student student=new Student(1,"Maha",22,30000);
 		studentService.addStudent(student);
 		verify(studentRepo,times(1)).save(student);
-     }
+	}
+
+	@Test
+	public void getAllStudentTest() {
+		when(studentRepo.findAll()).thenReturn(Stream
+				.of(new Student(1,"Maha", 22,30000),
+						new Student(2,"Harshi",23,40000))
+				.collect(Collectors.toList()));
+		assertEquals(2,studentService.getAllStudent().size());
+	}
+	@Test
+	public void updateStudentTest() {
+		long sId=2;
+		Student student=new Student(2,"Maharshi",23,40000);
+		studentService.updateStudent(sId, student);
+		verify(studentRepo,times(1)).save(student);
+	}
+
+	@Test
+	public void deleteStudentTest() {
+		long sId=2;
+		studentService.deleteStudent(sId);
+		verify(studentRepo,times(1)).deleteById(sId);
+	}
 
 
 }
